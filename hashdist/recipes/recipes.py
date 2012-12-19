c170e111c94c12cd75a94ff3138f077e807159b4
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-12-19 15:32:41 +0100

    WIP from emacs: recipes.py

--- hashdist/recipes/recipes.py
+++ hashdist/recipes/recipes.py
@@ -223,7 +223,7 @@ def order_by_constraints(problem):
     The concrete algorithm is to first invert the DAG (each object knows
     which ones it should come before), then start at the roots of this
     DAG and form sub-trees using DFS (visiting children in order sorted
-    by their key); then sort 
+    by their key); then finally 
     
     
     where we require that ``
