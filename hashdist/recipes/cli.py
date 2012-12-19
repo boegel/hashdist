e8de2ee5634276b46120968109bbab9576bce9b8
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-12-19 14:52:12 +0100

    WIP from emacs: cli.py

--- hashdist/recipes/cli.py
+++ hashdist/recipes/cli.py
@@ -18,7 +18,7 @@ def stack_script_cli(root_recipe):
     parser.add_argument('--config',
                         default=os.path.expanduser(DEFAULT_CONFIG_FILENAME),
                         help='location of Hashdist config-file (default: %s))' % DEFAULT_CONFIG_FILENAME)
-    parser.add_argument('-k', '--keep', choices=['never', 'always', 'error'],
+    parser.add_argument('-k', '--keep', choices=['never', 'always', 'error'], default='error,
                         help='when to keep build directories')
     parser.add_argument('-n', '--never-keep', action='store_true',
                         help='never keep temporary build directories')
